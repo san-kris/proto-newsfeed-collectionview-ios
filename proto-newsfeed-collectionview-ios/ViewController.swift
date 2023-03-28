44a372de22f2e1209226a7b645e925bd2883b9f1
Santosh Krishnamurthy <san.b350@gmail.com>
2023-03-28 09:14:22 -0500

    Initial Commit

---
+++ proto-newsfeed-collectionview-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proto-newsfeed-collectionview-ios
+//
+//  Created by Santosh Krishnamurthy on 3/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
